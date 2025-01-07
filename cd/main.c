e78a2d535f95a70f80306724468c871fb6607050
Ilan Nowak--Glandier <inowak--@2E6.42angouleme.fr>
2025-01-07 16:13:04 +0100

    cd

--- cd/main.c
+++ cd/main.c
@@ -6,95 +6,68 @@
 /*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 07:56:31 by inowak--          #+#    #+#             */
-/*   Updated: 2025/01/07 14:29:49 by inowak--         ###   ########.fr       */
+/*   Updated: 2025/01/07 16:08:36 by inowak--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-# define BUFFER_SIZE 100000
+#define BUFFER_SIZE 100000
 
-char *ft_clear_quote(char *input)
+char *ft_pwd(char **argv, char **env)
 {
-	int i;
-	int	j;
-	char *result;
+    char buffer[BUFFER_SIZE];
+    char *path;
 
-	j = 0;
-	i = 0;
-	while(input[i] != '"' && input[i])
-		i++;
-	if (input[i] == '"')
-	{
-		i++;
-		j = i;
-		while(input[i + 1] != '"' && input[i])
-			i++;
-		result = ft_substr(input, j, i);
-		// printf("result:%s | input:%s | j:%d | i:%d\n", result, input, j, i);
-		int re = ft_strncmp(result, "pwd", i);
-		if (re)
-		{
-			printf("%s: too many argument5s\n", input);
-				return (NULL);
-		}
-	}
-	return (ft_strtrim(input ,"\""));
+    if (!ft_strncmp(argv[0], "cd", ft_strlen(argv[0])))
+    {
+        if (!getcwd(buffer, BUFFER_SIZE))
+        {
+            perror("Error retrieving directory");
+            if (errno == ERANGE)
+                printf("BUFFER_SIZE too small\n");
+            return NULL;
+        }
+        path = strdup(buffer);
+        if (!path)
+        {
+            perror("Error allocating memory for path");
+            return NULL;
+        }
+        return (path);
+    }
+
+    if (!access(ft_strjoin("/usr/bin/", argv[0]), X_OK))
+    {
+        if (fork() == 0)
+            execve(ft_strjoin("/usr/bin/", argv[0]), argv, env);
+        else
+            wait(NULL);
+    }
+    return NULL;
 }
 
-char	**ft_argv(char *input)
+
+void	ft_cd(char **argv, char **env)
 {
-	int		i;
-	int		j;
-	int		k;
-	int		t;
-	char	**argv;
+	char	*root;
+	char	*path;
+	char	*target_path;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	t = 0;
-	input = ft_clear_quote(input);
-	if (!input)
-		return (NULL);
-	argv = malloc(sizeof(char *) * (ft_words(input, ' ') + 1));
-	if (!argv)
-		return (NULL);
-	while (input[i])
+	root = ft_pwd(argv, env);
+	if (root)
 	{
-		k = 0;
-		t = 0;
-		while (input[i] == ' ' && input[i])
-			i++;
-		k = i;
-		while (input[i] != ' ' && input[i])
+		path = ft_strjoin(root, "/");
+		free(root);
+		target_path = ft_strjoin(path, argv[1]);
+		free(path);
+		if (chdir(target_path))
 		{
-			i++;
-			t++;
-		}
-		argv[j] = ft_substr(input, k, t);
-		if (!argv[j])
-			return (NULL);
-		j++;
-	}
-	argv[j] = NULL;
-	return (argv);
-}
-
-void	ft_pwd(char **argv)
-{
-	char buffer[BUFFER_SIZE];
-
-	if (!ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])))
-	{
-		if (getcwd(buffer, BUFFER_SIZE) == NULL)
-		{
-			printf("Error directory\n");
-			if (errno == ERANGE)
-				printf("BUFFER_SIZE too small\n");
+			free(target_path);
+			perror("2Error");
 			return ;
 		}
-		printf("%s\n", buffer);
+		free(target_path);
 	}
 }
 
@@ -109,10 +82,10 @@ void	ft_free_tab(char **table)
 	table = NULL;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
-	char *input;
-	
+	char	*input;
+
 	if (argc != 1)
 	{
 		printf("Error input\n");
@@ -122,17 +95,18 @@ int main(int argc, char **argv)
 	while (input)
 	{
 		input = readline("Minishell> ");
-		if (input)
+		if (input && input[0] != '\0')
 		{
 			add_history(input);
-			argv = ft_argv(input);
+			argv = ft_split(input, ' ');
 			if (argv)
 			{
-				ft_pwd(argv);
+				ft_cd(argv, env);
 				ft_free_tab(argv);
 				argv = NULL;
 			}
-			free(input);
 		}
+		if (input)
+			free(input);
 	}
 }
