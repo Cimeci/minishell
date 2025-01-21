69e59866e87c91f608dbc74d8115d03cc2f290ae
Ilan Nowak--Glandier <inowak--@2C6.42angouleme.fr>
2025-01-21 10:28:58 +0100

    signal

--- signal/main.c
+++ signal/main.c
@@ -6,20 +6,20 @@
 /*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 09:10:04 by inowak--          #+#    #+#             */
-/*   Updated: 2025/01/20 17:21:20 by inowak--         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:21:07 by inowak--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-pid_t	child_pid = -1;
+pid_t		g_child_pid = -1;
 
 void	child_signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		printf("\n");
-		exit(0);
+		write(1, "\n", 1);
+		exit(1);
 	}
 }
 
@@ -27,9 +27,9 @@ void	parent_signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		if (child_pid <= 0)
+		if (g_child_pid <= 0)
 		{
-			printf("\n");
+			write(1, "\n", 1);
 			rl_on_new_line();
 			rl_replace_line("", 0);
 			rl_redisplay();
@@ -37,53 +37,64 @@ void	parent_signal_handler(int signum)
 	}
 }
 
-static void	handle_child_process(void)
-{
-	char	*child_input;
-
-	signal(SIGINT, child_signal_handler);
-	child_input = "1";
-	while (child_input)
-	{
-		child_input = readline("Heredoc> ");
-		if (child_input)
-			free(child_input);
-	}
-}
-
-static void	handle_run_command(void)
-{
-	child_pid = fork();
-	if (child_pid < 0)
-	{
-		perror("Erreur lors du fork");
-		return ;
-	}
-	if (child_pid == 0)
-		handle_child_process();
-	else
-	{
-		waitpid(child_pid, NULL, 0);
-		child_pid = -1;
-	}
-}
-
 static void	handle_exit_command(char *input)
 {
-	if (child_pid > 0)
+	if (g_child_pid > 0)
 	{
-		kill(child_pid, SIGINT);
-		waitpid(child_pid, NULL, 0);
+		kill(g_child_pid, SIGINT);
+		waitpid(g_child_pid, NULL, 0);
 	}
 	free(input);
 	clear_history();
 }
 
+static void	handle_child_process(void)
+{
+	char	*child_input;
+
+	signal(SIGINT, child_signal_handler);
+	child_input = NULL;
+	while (1)
+	{
+		child_input = readline("Heredoc> ");
+		if (!child_input)
+		{
+			exit(1);
+		}
+		if (!ft_strncmp(child_input, "exit", 4) && ft_strlen(child_input) == 4)
+		{
+			handle_exit_command(child_input);
+			exit(0);
+		}
+		printf("%s\n", child_input);
+		free(child_input);
+	}
+}
+
+static void	handle_run_command(void)
+{
+	int	status;
+
+	g_child_pid = fork();
+	if (g_child_pid < 0)
+	{
+		perror("Erreur lors du fork");
+		return ;
+	}
+	if (g_child_pid == 0)
+		handle_child_process();
+	else
+	{
+		waitpid(g_child_pid, &status, 0);
+		g_child_pid = -1;
+	}
+}
+
 static void	process_command(char *input)
 {
-	if (!strcmp(input, "run"))
+	if (!ft_strncmp(input, "run", 3) && ft_strlen(input) == 3)
 		handle_run_command();
-	else if (!strcmp(input, "exit"))
+	else if (!ft_strncmp(input, "exit", 4) && ft_strlen(input) == 4)
 	{
 		handle_exit_command(input);
 		exit(0);
@@ -96,18 +107,21 @@ int	main(void)
 {
 	char	*input;
 
-	input = "1";
 	signal(SIGINT, parent_signal_handler);
-	while (input)
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
 	{
 		input = readline("minishell> ");
-		if (input)
+		if (!input)
 		{
-			add_history(input);
-			process_command(input);
-			if (strcmp(input, "exit") != 0)
-				free(input);
+			printf("exit\n");
+			exit(0);
 		}
+		if (*input)
+			add_history(input);
+		process_command(input);
+		if (strcmp(input, "exit") != 0)
+			free(input);
 	}
 	return (0);
 }
