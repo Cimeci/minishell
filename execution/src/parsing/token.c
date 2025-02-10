edff5b10f51e8d81813b75cdd5db8271d14719b8
Noa Charbogne <noa.charbogne@orange.fr>
2025-02-10 09:59:51 +0100

    avancement

--- execution/src/parsing/token.c
+++ execution/src/parsing/token.c
@@ -6,7 +6,7 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 10:34:49 by ncharbog          #+#    #+#             */
-/*   Updated: 2025/02/06 14:14:46 by ncharbog         ###   ########.fr       */
+/*   Updated: 2025/02/10 09:41:12 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,17 +111,18 @@ void	get_token(char *str, t_token *cur)
 
 	len = get_token_len(str);
 	only_token = ft_substr(str, 0, len);
-	if (ft_strnstr(str, "|", len))
+	if (ft_strlen(only_token) == 1 && ft_strncmp(str, "|", len))
 		cur->type = PIPE;
-	else if (ft_strnstr(str, "<<", len))
+	else if (ft_strlen(only_token) == 2 && ft_strncmp(str, "<<", len))
 		cur->type = HEREDOC;
-	else if (ft_strnstr(str, "<", len))
+	else if (ft_strlen(only_token) == 1 && ft_strncmp(str, "<", len))
 		cur->type = INPUT;
-	else if (ft_strnstr(str, ">>", len))
+	else if (ft_strlen(only_token) == 2 && ft_strncmp(str, ">>", len))
 		cur->type = APPEND;
-	else if (ft_strnstr(str, ">", len))
+	else if (ft_strlen(only_token) == 1 && ft_strncmp(str, ">", len))
 		cur->type = OVERWRITE;
-	else if (ft_strnstr(str, "''", len) || ft_strnstr(str, "\"\"", len))
+	else if (ft_strlen(only_token) == 2
+		&& (ft_strncmp(str, "''", len) || ft_strncmp(str, "\"\"", len)))
 		cur->type = EMPTY_QUOTE;
 	else
 		cur->type = WORD;
