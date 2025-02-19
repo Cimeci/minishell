562d5569c500792081c72913651509a5511af9ef
Ilan Nowak--Glandier <inowak--@2D6.42angouleme.fr>
2025-02-19 10:50:33 +0100

    for merge

--- execution/src/parsing/main.c
+++ execution/src/parsing/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
+/*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:33:13 by ncharbog          #+#    #+#             */
-/*   Updated: 2025/02/18 17:22:57 by ncharbog         ###   ########.fr       */
+/*   Updated: 2025/02/19 09:58:38 by inowak--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,8 @@ int	parsing(t_data *data, char *input)
 				cur_tok = cur_tok->next;
 			}
 			handle_here_doc(data, cur_cmd);
-			setup_child_process(data, cur_cmd);
+			if (cur_cmd->here == 1)
+				ft_heredoc(data, cur_cmd);
 			cur_cmd = cur_cmd->next;
 		}
 		signal(SIGINT, SIG_DFL);
